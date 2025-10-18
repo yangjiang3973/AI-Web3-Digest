5d964df6f5e7f11b61aacc2d04e1fe8d5597a73c
yangjiang3973 <yang3973@gmail.com>
2025-10-18 16:43:33 +0800

    e2e test

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,7 @@ async function main() {
 }
 
 cron.schedule(
-    '21 16 * * *',
+    '00 10 * * *',
     async () => {
         console.log('Running the bot at 10:00 AM every day');
         await main();
